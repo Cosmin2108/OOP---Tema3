b38096a1ff37d04ec29e217c44a6217bf677f375
Cosmin2108 <cosmin.stefan952@yahoo.com>
2020-01-21 17:27:41 +0200

    Update casa.h

--- casa.h
+++ casa.h
@@ -3,21 +3,21 @@
 
 
 
-#include"Locuinta.h"
+#include "Locuinta.h"
 #include <iostream>
-#include<vector>
+#include <vector>
 #include <typeinfo>
-#include<string.h>
-#include<windows.h>
-#include<stdlib.h>
-#include<conio.h>
+#include <string.h>
+#include <windows.h>
+#include <stdlib.h>
+#include <conio.h>
 
 class Casa: public Locuinta
 {
 private:
 	int nr_etaj;
 	double area_yard;/// suprafata curte
-    std::vector<double> useful_area;
+    	std::vector<double> useful_area;
 public:
 	Casa(){}
 	Casa(int nr_etaj, double area_yard, std::vector<double> useful_area1, int discount,const char *name):Locuinta(discount,name), nr_etaj(nr_etaj), area_yard(area_yard){ useful_area.assign(useful_area1.begin(), useful_area1.end());}
@@ -26,10 +26,10 @@ public:
 
 	Casa& operator = (Casa &Ob);
 	friend std::istream& operator >>(std::istream &in, Casa &ob);
-    friend std::ostream& operator <<(std::istream &out, Casa &ob);
+    	friend std::ostream& operator <<(std::istream &out, Casa &ob);
 
-    void citire();
-    void afisare();
+	void citire();
+	void afisare();
 	double calculate_rent();
 
 };
